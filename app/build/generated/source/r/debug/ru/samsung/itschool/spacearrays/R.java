46cb6e4c8a994763193bfa1342b3a2e040d36908
chopsticks12 <kurilenkodanil@gmail.com>
2017-12-10 21:44:33 +0300

    SpaceShuttle

--- app/build/generated/source/r/debug/ru/samsung/itschool/spacearrays/R.java
+++ app/build/generated/source/r/debug/ru/samsung/itschool/spacearrays/R.java
@@ -14,9 +14,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
         public static final int rocket=0x7f020001;
         public static final int spaceshuttleicon=0x7f020002;
+        public static final int supretyty=0x7f020003;
+        public static final int tyty=0x7f020004;
     }
     public static final class id {
+        public static final int button=0x7f060001;
         public static final int myDraw1=0x7f060000;
+        public static final int myImage=0x7f060002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
